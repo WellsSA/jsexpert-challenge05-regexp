e820444d3c4c3fed3d05d6092d29dba414063a61
WellsSA <wel.cavzod@gmail.com>
2022-04-07 01:54:26 -0300

    feat(src): setting up project files

---
+++ src/index.js
@@ -0,0 +1,2 @@
+// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+// Dica do wells: no lugar da chamada do PDF parser, um simples `.toString()` resolve, já que nós é que implementaremos o "CSV parser"
---
+++ src/textProcessorFluentAPI.js
@@ -0,0 +1,1 @@
+// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
